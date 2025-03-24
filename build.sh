ae169ce6b2c83b61e38c1bceec89ba398d56315b
Segundo <segundo.cofinco@gmail.com>
2025-03-24 11:31:12 -0300

    first commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/user/bin/env bash
+# exit on error
+set -o errexit
+
+# poetry install
+pip install -r requeriments.txt
+
+python manage.py collectstatic --mo-input
+python manage.py migrategit ini
